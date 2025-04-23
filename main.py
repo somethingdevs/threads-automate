a14cc6431da130e6df6bad1b1f6e7fff13058c50
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2025-04-23 02:27:55 -0500

    Correct import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from threads_routes import router as threads_router
+from thread_routes import router as threads_router
 
 app = FastAPI()
 app.include_router(threads_router)
